f7296b3f0f28437968f3355e94f405bddd56dac5
strayacode <33187392+strayacode@users.noreply.github.com>
2019-06-03 23:36:54 +1000

    Update music.py

--- music.py
+++ music.py
@@ -5,6 +5,7 @@ font = "inconsolata"
 root = Tk()
 root.minsize(800, 500)
 pygame.mixer.init()
+root.configure(background="#191919")
 files = []
 path = "/home/straya/Downloads/"
 played = False
@@ -24,6 +25,8 @@ def songselect(event):
 	pygame.mixer.music.play()
 	played = True
 	state = "playing"
+	dynamic_title.set(files[index][:-4])
+	dynamic_control.set("Pause")
 
 def play_pause_control():
 	global state
@@ -33,16 +36,24 @@ def play_pause_control():
 			pygame.mixer.music.play()
 			played = True
 			state = "playing"
+			dynamic_title.set(files[index][:-4])
+			dynamic_control.set("Pause")
 		elif played == True:
 			pygame.mixer.music.unpause()
 			state = "playing"
+			dynamic_title.set(files[index][:-4])
+			dynamic_control.set("Pause")
 	elif state == "playing":
 		if played == True:
 			pygame.mixer.music.pause()
 			state = "paused"
+			dynamic_title.set("")
+			dynamic_control.set("Play")
 
 def previous_song():
 	global index
+	global state
+	global played
 	index -= 1
 	if index == len(files):
 		index = 0
@@ -50,9 +61,23 @@ def previous_song():
 		index = len(files) - 1
 	pygame.mixer.music.load(files[index])
 	pygame.mixer.music.play()
+	dynamic_title.set(files[index][:-4])
+	state = "playing"
+	played = True
+	dynamic_control.set("Pause")
+	if index == len(files) - 1:
+		songs.select_clear(0)
+	else:
+		songs.select_clear(index + 1)
+	
+	songs.select_set(index)
+	songs.event_generate("<<ListboxSelect>>")
+	
 
 def next_song():
 	global index
+	global state
+	global played
 	index += 1
 	if index == len(files):
 		index = 0
@@ -60,19 +85,35 @@ def next_song():
 		index = len(files) - 1
 	pygame.mixer.music.load(files[index])	
 	pygame.mixer.music.play()
+	dynamic_title.set(files[index][:-4])
+	state = "playing"
+	played = True
+	dynamic_control.set("Pause")
+	if index == 0:
+		songs.select_clear(len(files) - 1)
+	else:
+		songs.select_clear(index - 1)
+	songs.select_set(index)
+	songs.event_generate("<<ListboxSelect>>")
 
 
 
 # Placing widgets
-songs = Listbox(root, width=100, font="{} 12".format(font))
+dynamic_control = StringVar()
+dynamic_control.set("Play")
+dynamic_title = StringVar()
+song_title = Label(root, font="{} 12 bold".format(font), textvariable=dynamic_title, background="#191919", foreground="#ffffff")
+song_title.place(x=0, y=400)
+songs = Listbox(root, width=100, font="{} 12 bold".format(font), background="#191919", foreground="#ffffff", highlightthickness=0, borderwidth=-1,selectborderwidth=0, selectbackground="#639fff", selectforeground="#ffffff", relief=FLAT)
 songs.bind("<<ListboxSelect>>", songselect)
 for song in files:
 	songs.insert(END, song)
 songs.place(x=0, y=0)
 next_button = Button(root, width=10, text="Next", command=next_song, font="{} 12".format(font))
 next_button.place(x=0, y=300)
-play_button = Button(root, width=10, text="Play", command=play_pause_control, font="{} 12".format(font))
+play_button = Button(root, width=10, text="Play", command=play_pause_control, font="{} 12".format(font), textvariable=dynamic_control)
 play_button.place(x=100, y=300)
 previous_button = Button(root, width=10, text="Previous", command=previous_song, font="{} 12".format(font))
 previous_button.place(x=200, y=300)
+
 root.mainloop()
